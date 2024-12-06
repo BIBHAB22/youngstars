a90b47322bea30771408999d19399c33a6a8ed25
BIBHAB22 <bibhabindia@gmail.com>
2024-12-06 20:33:10 +0530

    hamburger update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,31 @@
-// Navbar.js
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = ({ isLoggedIn, onLogout }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <h2>Youngstars</h2>
       </div>
-      <div className="navbar-right">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`navbar-right ${menuOpen ? "open" : ""}`}>
         <Link to="/">Home</Link>
         <Link to="/add-player">Register</Link>
         <Link to="/scoreboard">Scoreboard</Link>
-          {isLoggedIn ? (
-            <button onClick={onLogout}>Logout</button>
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        
+        {isLoggedIn ? (
+          <button onClick={onLogout}>Logout</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </div>
     </nav>
   );
