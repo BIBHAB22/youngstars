af58b45f243a3369322c5d32854ad4b4103c9a8f
BIBHAB22 <bibhabindia@gmail.com>
2024-12-18 17:42:09 +0530

    gitignore added

--- src/server.js
+++ src/server.js
@@ -18,11 +18,11 @@ app.use(bodyParser.json());
 
 // MySQL connection setup
 const db = mysql.createConnection({
-  host: "mysql-7c741d8-my-first-project-2023.a.aivencloud.com",
-  port: 28546,
-  user: "avnadmin",
-  password: "AVNS_kH6UkI3YsxGLfZxeMK5",
-  database: "defaultdb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
